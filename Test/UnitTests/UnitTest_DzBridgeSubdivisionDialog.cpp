b344450c74bc394e7a8dc78dba37ed6eb26b0f35
danielbui78 <danielbui78@gmail.com>
2022-02-23 10:31:13 -0500

    UnitTests: Dialog, MorphSelection, Subdivision, part 2

--- Test/UnitTests/UnitTest_DzBridgeSubdivisionDialog.cpp
+++ Test/UnitTests/UnitTest_DzBridgeSubdivisionDialog.cpp
@@ -3,6 +3,10 @@
 #include "UnitTest_DzBridgeSubdivisionDialog.h"
 #include "DzBridgeSubdivisionDialog.h"
 
+UnitTest_DzBridgeSubdivisionDialog::UnitTest_DzBridgeSubdivisionDialog()
+{
+	m_testObject = (QObject*) new DzBridgeSubdivisionDialog();
+}
 
 bool UnitTest_DzBridgeSubdivisionDialog::runUnitTests()
 {
@@ -13,9 +17,91 @@ bool UnitTest_DzBridgeSubdivisionDialog::runUnitTests()
 		return false;
 	}
 
+	RUNTEST(_DzBridgeSubdivisionDialog);
+	RUNTEST(getSubdivisionCombos);
+	RUNTEST(PrepareDialog);
+	RUNTEST(LockSubdivisionProperties);
+	RUNTEST(WriteSubdivisions);
+	RUNTEST(FindObject);
+	RUNTEST(setSubdivisionLevelByNode);
+	RUNTEST(UnlockSubdivisionProperties);
+	RUNTEST(GetLookupTable);
+	RUNTEST(HandleSubdivisionLevelChanged);
+
 	return true;
 }
 
-#include "moc_UnitTest_DzBridgeSubdivisionDialog.cpp"
+bool UnitTest_DzBridgeSubdivisionDialog::_DzBridgeSubdivisionDialog(UnitTest::TestResult* testResult)
+{
+	bool bResult = true;
+	TRY_METHODCALL(new DzBridgeSubdivisionDialog());
+	return bResult;
+}
 
+bool UnitTest_DzBridgeSubdivisionDialog::getSubdivisionCombos(UnitTest::TestResult* testResult)
+{
+	bool bResult = true;
+	TRY_METHODCALL(qobject_cast<DzBridgeSubdivisionDialog*>(m_testObject)->getSubdivisionCombos());
+	return bResult;
+}
+
+bool UnitTest_DzBridgeSubdivisionDialog::PrepareDialog(UnitTest::TestResult* testResult)
+{
+	bool bResult = true;
+	TRY_METHODCALL(qobject_cast<DzBridgeSubdivisionDialog*>(m_testObject)->PrepareDialog());
+	return bResult;
+}
+
+bool UnitTest_DzBridgeSubdivisionDialog::LockSubdivisionProperties(UnitTest::TestResult* testResult)
+{
+	bool bResult = true;
+	TRY_METHODCALL(qobject_cast<DzBridgeSubdivisionDialog*>(m_testObject)->LockSubdivisionProperties(false));
+	return bResult;
+}
+
+bool UnitTest_DzBridgeSubdivisionDialog::WriteSubdivisions(UnitTest::TestResult* testResult)
+{
+	bool bResult = true;
+	DzJsonWriter arg(nullptr);
+	TRY_METHODCALL(qobject_cast<DzBridgeSubdivisionDialog*>(m_testObject)->WriteSubdivisions(arg));
+	return bResult;
+}
+
+bool UnitTest_DzBridgeSubdivisionDialog::FindObject(UnitTest::TestResult* testResult)
+{
+	bool bResult = true;
+	TRY_METHODCALL_NULLPTR(qobject_cast<DzBridgeSubdivisionDialog*>(m_testObject)->FindObject(nullptr, ""));
+	return bResult;
+}
+
+bool UnitTest_DzBridgeSubdivisionDialog::setSubdivisionLevelByNode(UnitTest::TestResult* testResult)
+{
+	bool bResult = true;
+	TRY_METHODCALL_NULLPTR(qobject_cast<DzBridgeSubdivisionDialog*>(m_testObject)->setSubdivisionLevelByNode(nullptr, 0));
+	return bResult;
+}
+
+bool UnitTest_DzBridgeSubdivisionDialog::UnlockSubdivisionProperties(UnitTest::TestResult* testResult)
+{
+	bool bResult = true;
+	TRY_METHODCALL(qobject_cast<DzBridgeSubdivisionDialog*>(m_testObject)->UnlockSubdivisionProperties());
+	return bResult;
+}
+
+bool UnitTest_DzBridgeSubdivisionDialog::GetLookupTable(UnitTest::TestResult* testResult)
+{
+	bool bResult = true;
+	TRY_METHODCALL(qobject_cast<DzBridgeSubdivisionDialog*>(m_testObject)->GetLookupTable());
+	return bResult;
+}
+
+bool UnitTest_DzBridgeSubdivisionDialog::HandleSubdivisionLevelChanged(UnitTest::TestResult* testResult)
+{
+	bool bResult = true;
+	TRY_METHODCALL(qobject_cast<DzBridgeSubdivisionDialog*>(m_testObject)->HandleSubdivisionLevelChanged(""));
+	return bResult;
+}
+
+
+#include "moc_UnitTest_DzBridgeSubdivisionDialog.cpp"
 #endif
